319c7cc5b248612ce2bb72b343fa5d6dd239f5be
zxb5102 <zxb5102@163.com>
2018-10-08 11:23:56 +0800

    '动画效果'

--- src/pages/index/js/index.js
+++ src/pages/index/js/index.js
@@ -1,4 +1,13 @@
 var video = document.querySelector('.top-video');
+video.addEventListener("transitionend", function(event) {
+    video.classList.add('d-none');
+  }, false);
 video.addEventListener('playing',function(){
     //视频已经开始播放了
+    var hideTime = 4*1000;
+    var totalTime = 16*1000;
+    var offsetTime = totalTime - hideTime;
+    window.setTimeout(()=>{
+        video.classList.add('video-hide');
+    },offsetTime);
 });
