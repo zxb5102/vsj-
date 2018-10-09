d35d0d429627d669b6529493750f48bf00be59cb
zxb5102 <zxb5102@163.com>
2018-10-09 13:40:18 +0800

    '更新动画效果'

--- src/pages/index/js/index.js
+++ src/pages/index/js/index.js
@@ -1,29 +1,48 @@
 var video = document.querySelector('.top-video');
 var videoBtn = document.querySelector('.video-close-btn');
 var videoFont = document.querySelector('.top-video-font');
+var videoFontEnd = document.querySelector('.top-video-font-end');
 
 var videoFontTime = 4;
 document.documentElement.style.setProperty('--video-font-time', `${videoFontTime}s`);
 
-video.addEventListener("transitionend", function(event) {
+video.addEventListener("transitionend", function (event) {
     video.classList.add('d-none');
-  }, false);
-video.addEventListener('playing',function(){
+}, false);
+video.addEventListener('playing', function () {
     videoFont.classList.add('top-video-font-opacity-show');
     //视频已经开始播放了
-    var hideTime = 4*1000;
-    var totalTime = 16*1000;
+    var hideTime = 4 * 1000;
+    var totalTime = 16 * 1000;
     var offsetTime = totalTime - hideTime;
-    window.setTimeout(()=>{
+    window.setTimeout(() => {
         video.classList.add('video-hide');
         videoBtn.classList.add('d-none');
-    },offsetTime);
-    window.setTimeout(()=>{
-        videoFont.classList.remove('top-video-font-opacity-show');
-    },totalTime - videoFontTime * 1000);
+    }, offsetTime);
+
+    var stayTime = 0.5;
+    window.setTimeout(() => {
+        // videoFont.classList.remove('top-video-font-opacity-show');
+        window.setTimeout(()=>{
+            videoFontEnd.classList.add('top-video-font-end-opacity-show');
+        }, (videoFontTime - 3.5)* 1000)
+        videoFont.style.opacity = 0;
+    }, (videoFontTime + stayTime) * 1000);
+
+    //结尾文字的消失
+    window.setTimeout(() => {
+        videoFontEnd.style.opacity = 0;
+    }, ( totalTime - 4*1000 )   );
+    // window.setTimeout(()=>{
+    //     videoFont.classList.remove('top-video-font-opacity-show');
+    // },( videoFontTime*2 + stayTime  ) * 1000);
+
+    // window.setTimeout(()=>{
+    //     videoFontEnd.classList.remove('top-video-font-end-opacity-show');
+    // },( videoFontTime*2 + stayTime*2) * 1000);
 });
-videoBtn.addEventListener('click',()=>{
-        video.classList.add('d-none');
-        videoBtn.classList.add('d-none');
-        videoFont.classList.add('d-none');
+videoBtn.addEventListener('click', () => {
+    video.classList.add('d-none');
+    videoBtn.classList.add('d-none');
+    videoFont.classList.add('d-none');
 });
